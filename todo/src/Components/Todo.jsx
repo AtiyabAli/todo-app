67cd6110cad3cb7925f03c6f096d310f3661f1f2
AtiyabAli <syedatiyabali@gmail.com>
2024-04-04 03:09:43 +0530

    layout-done

---
+++ todo/src/Components/Todo.jsx
@@ -0,0 +1,14 @@
+import './CSS/Todo.css'
+
+const Todo = () => {
+  return (
+    <div className='todo'>
+        <div className="todo-header">
+            To-Do List
+        </div>
+      
+    </div>
+  )
+}
+
+export default Todo
